8d3cd3a6d36c0ede77059aeb5106d134feb23ed6
Lorax55 <vladyslav.usa@gmail.com>
2025-05-19 17:35:13 -0700

    Updated to include SRC file
    Including ALL files, 
    without "idea" file, /gradle/wrapper/gradle-wrapper.jar, radlew.bat, and gradlew

---
+++ src/main/java/com/example/linkchat/linkchat.java
@@ -0,0 +1,13 @@
+package com.example.linkchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class linkchat {
+
+	public static void main(String[] args) {
+		SpringApplication.run(linkchat.class, args);
+	}
+
+}
